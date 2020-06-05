c689db93def13603504bc85df5f6fa57ef53b3af
zoranildosantos <zoranildo22@gmail.com>
2020-06-04 22:52:52 -0300

    Conclusão segundo dia

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,25 +1,16 @@
 import express from 'express'
-import knex from './database/connection'
 
 import PointsController from './controllers/PointsControllers'
+import ItemsController from './controllers/ItemsControllers'
 
 const routes = express.Router()
 const pointsController = new PointsController()
+const itemsController = new ItemsController()
 
-routes.get('/items', async (req, res) => {
-  const items = await knex('items').select('*')
-
-  const serializedItems = items.map(item => {
-    return {
-      id: item.id,
-      title: item.title,
-      image_url: `http://localhost:3333/uploads/${item.image}`
-    }
-  })
-
-  return res.json(serializedItems)
-})
+routes.get('/items', itemsController.index)
 
 routes.post('/points', pointsController.create)
+routes.get('/points', pointsController.index)
+routes.get('/points/:id', pointsController.show)
 
-  export default routes
+export default routes
