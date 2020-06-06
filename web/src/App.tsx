7d33ce29014e3f47fc5d7236fe1e1c5d1bc7fb37
zoranildosantos <zoranildo22@gmail.com>
2020-06-06 16:16:41 -0300

    Terceiro dia | Primeira parte

---
+++ web/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css';
+
+import Routes from './routes'
+
+function App() {
+  return (
+    <div>
+      <Routes />
+    </div>
+  );
+}
+
+export default App;
+// useState retorna um array com dua posições [ valor do estado, função para atualizar o valor do estado]
